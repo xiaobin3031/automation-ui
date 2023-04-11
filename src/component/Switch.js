df9eda95b9b106bd740fc33083b3d1d4e66e45fe
weibin.xu <xuwb@shanshan-business.com>
2023-04-11 22:45:28 +0800

    优化

--- src/component/Switch.js
+++ src/component/Switch.js
@@ -1,7 +1,7 @@
 import { useRef, useState } from "react"
 import '../css/switch.css'
 
-const comBtnBorderClass = 'x-switch-button-border', comBtnClass = 'x-switch-button';
+const comBtnBorderClass = 'x-switch-button-border', comBtnClass = 'x-switch-button', comLabelClass = "x-switch-label";
 export default function Switch({
   trueName="ok", 
   trueColor="green", 
@@ -14,16 +14,22 @@ export default function Switch({
 ){
 
   const curVal = useRef(currentValue);
-  let _btnClass, _borderClass;
+  let _btnClass, _borderClass, _labelOkClass, _labelCancelClass;
   if(curVal.current === okValue){
     _btnClass = [comBtnClass, 'x-switch-button-ok'];
     _borderClass = [comBtnBorderClass, 'x-switch-button-border-ok'];
+    _labelOkClass = [comLabelClass, 'x-switch-label-ok'];
+    _labelCancelClass = [comLabelClass];
   }else{
     _btnClass = [comBtnClass, 'x-switch-button-cancel'];
     _borderClass = [comBtnBorderClass, 'x-switch-button-border-cancel'];
+    _labelOkClass = [comLabelClass];
+    _labelCancelClass = [comLabelClass, 'x-switch-label-cancel'];
   }
   const [btnClass, setBtnClass] = useState(_btnClass);
   const [borderClass, setBorderClass] = useState(_borderClass);
+  const [labelOkClass, setLabelOkClass] = useState(_labelOkClass);
+  const [labelCancelClass, setLabelCancelClass] = useState(_labelCancelClass);
 
   function toggleSwitch(){
     if(curVal.current === okValue){
@@ -34,19 +40,23 @@ export default function Switch({
     if(curVal.current === okValue){
       setBtnClass([comBtnClass, 'x-switch-button-ok-animation']);
       setBorderClass([comBtnBorderClass, 'x-switch-button-border-ok-animation']);
+      setLabelOkClass([comLabelClass, 'x-switch-label-ok']);
+      setLabelCancelClass([comLabelClass]);
     }else{
       setBtnClass([comBtnClass, 'x-switch-button-cancel-animation']);
       setBorderClass([comBtnBorderClass, 'x-switch-button-border-cancel-animation']);
+      setLabelOkClass([comLabelClass]);
+      setLabelCancelClass([comLabelClass, 'x-switch-label-cancel']);
     }
   }
 
   return (
     <div className="x-switch">
-      <div className="x-switch-false-name x-switch-label" color={falseColor}>{falseName}</div>
+      <div className={labelOkClass.join(' ')} color={falseColor}>{falseName}</div>
       <div className={borderClass.join(' ')}>
         <div className={btnClass.join(' ')} onClick={toggleSwitch}></div>
       </div>
-      <div className="x-switch-true-name x-switch-label" color={trueColor}>{trueName}</div>
+      <div className={labelCancelClass.join(' ')} color={trueColor}>{trueName}</div>
     </div>
   )
 }
