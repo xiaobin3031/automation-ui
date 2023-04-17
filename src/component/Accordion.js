27af0d87e1ee9d8382075c7730dbfdcdfc00baef
weibin.xu <xuwb@shanshan-business.com>
2023-04-17 10:53:32 +0800

    accordion 添加只允许打开一个功能

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -4,6 +4,44 @@ import Icon from './Icon';
 
 let accordionGlobalId = 1;
 
+function collsaping($dom){
+  setAnimation(getHeaderIconDom($dom), 'x-accordion-header-close')
+  $dom.classList.remove('expand');
+  $dom.classList.add('collsaping');
+}
+
+function exapnding($dom){
+  setAnimation(getHeaderIconDom($dom), 'x-accordion-header-open')
+  $dom.classList.remove('collsape');
+  $dom.classList.add('expanding');
+}
+
+function getHeaderIconDom($dom){
+  const _children = $dom.children[0].children;
+  for(let i = 0;i<_children.length;i++){
+    if(_children[i].classList.contains('icon')){
+      return _children[i].firstChild;
+    }
+  }
+  return null;
+}
+
+function setAnimation($dom, name){
+  if(!$dom) return;
+  $dom.style.animation = `${name} .3s ease`
+  $dom.style.animationFillMode = 'forwards'
+}
+
+function contentTransitionEnd($dom){
+  if($dom.classList.contains('expanding')){
+    $dom.classList.remove('expanding');
+    $dom.classList.add('expand');
+  }else if($dom.classList.contains('collsaping')){
+    $dom.classList.remove('collsaping');
+    $dom.classList.add('collsape');
+  }
+}
+
 function AccordionHeader({title="Accortion Item", subTitle, isOpen}){
 
   let icon;
@@ -36,56 +74,31 @@ function AccordionContent({content}){
   )
 }
 
-function AccordionItem({title, subTitle, content, open, item, index, renderHeader, renderContent, openOne, collsapeOther}){
+function AccordionItem({title, subTitle, content, open, item, index, renderHeader, renderContent, collsapeOther}){
 
   const opens = useRef(!!item[open]);
   const _itemRef = useRef(null);
 
-  function contentTransitionEnd(){
-    if(opens.current){
-      _itemRef.current.classList.remove('expanding');
-      _itemRef.current.classList.add('expand');
-    }else{
-      _itemRef.current.classList.remove('collsaping');
-      _itemRef.current.classList.add('collsape');
-    } 
-  }
-
-  function setAnimation($dom, name){
-    if(!$dom) return;
-    $dom.style.animation = `${name} .3s ease`
-    $dom.style.animationFillMode = 'forwards'
-  }
-
-  function getHeaderIconDom(){
-    const _children = _itemRef.current.children[0].children;
-    for(let i = 0;i<_children.length;i++){
-      if(_children[i].classList.contains('icon')){
-        return _children[i].firstChild;
-      }
-    }
-    return null;
-  }
-
   function changeOpenState(){
-    opens.current = !opens.current;
-    if(opens.current){
-      setAnimation(getHeaderIconDom(), 'x-accordion-header-open')
-      _itemRef.current.classList.remove('collsape');
-      _itemRef.current.classList.add('expanding');
-    }else{
-      setAnimation(getHeaderIconDom(), 'x-accordion-header-close')
-      _itemRef.current.classList.remove('expand');
-      _itemRef.current.classList.add('collsaping');
+    const $dom = _itemRef.current;
+    if($dom.classList.contains('collsape')){
+      exapnding(_itemRef.current);
+      opens.current = true;
+    }else if($dom.classList.contains('expand')){
+      collsaping(_itemRef.current);
+      opens.current = false;
+    }
+    if(!!collsapeOther){
+      collsapeOther(index, opens.current);
     }
   }
 
   return (
     <div className={`item${opens.current ? ' expand' : ' collsape'}`} ref={_itemRef}>
-      <div className='header' onClick={() => changeOpenState()}>
+      <div className='header' onClick={changeOpenState}>
         {!!renderHeader ? renderHeader(item, index) : <AccordionHeader title={item[title]} subTitle={item[subTitle]} isOpen={opens.current}/>}
       </div>
-      <div className='content' onTransitionEnd={contentTransitionEnd}>
+      <div className='content' onTransitionEnd={event => contentTransitionEnd(event.target.parentNode)}>
         {!!renderContent ? renderContent(item, index) : <AccordionContent content={item[content]}/>}
       </div>
     </div>
@@ -99,12 +112,12 @@ export function Accordion({
   subTitle = "_subTitle",
   content = "_content",
   open = "_open",
+  openOne,
   renderHeader,
   renderContent,
   ...props}){
 
   const _accrodionRef = useRef(null);
-  const [_list, setList] = useState(list);
 
   function getItemKey(item){
     let _key = item[key];
@@ -112,6 +125,24 @@ export function Accordion({
       _key = `accortion-item-${accordionGlobalId++}`
     }
     return _key;
+  }
+
+  function collsapeOther(index, open){
+    if(!open){
+      return;
+    }
+    if(openOne){
+      const itemChildren = _accrodionRef.current.children;
+      for(let i=0;i<itemChildren.length;i++){
+        if(i === index){
+          continue;
+        }
+        const itemChild = itemChildren[i];
+        if(itemChild.classList.contains('expand')){
+          collsaping(itemChild);
+        }
+      }
+    }
   }
 
   return (
@@ -128,6 +159,7 @@ export function Accordion({
               open={open}
               index={index}
               openOne
+              collsapeOther={collsapeOther}
               renderContent={renderContent}
               renderHeader={renderHeader}
             />
